a8845e9e9ae584a61a158c4b98426483a16af7ba
flexirexi <felix_lehmann@ymail.com>
2025-07-10 03:00:30 +0200

    feat: add includes for navbars

--- my_project/settings.py
+++ my_project/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'allauth.account.middleware.AccountMiddleware',
+    # 'allauth.account.middleware.AccountMiddleware',  # caused a crash..
 ]
 
 ROOT_URLCONF = "my_project.urls"
