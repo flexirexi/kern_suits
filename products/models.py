ceb037366d9e40d41f3de8a3103c3a2a6dd29c38
flexirexi <felix_lehmann@ymail.com>
2025-07-13 03:06:36 +0200

    fead: add first models to product app
    Models: Category, Product and Series. For Suits, it is not enough to only have the product and the category
    . Also, the customer doesn't want to browse each part of the suit. So, there will be an option to browse
    for series/collection bundles. Moreover, a suit can be made in different colors, fits, fabrics...
    researching for a proper model..

--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+
+
+class Series(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name="products", on_delet=models.PROTECT)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    base_price = models.DecimalField(max_digits=8, decimal_places=2)
+    occasion = models.CharField(max_length=50, blank=True)
+    collection_name = models.CharField(max_length=100, blank=True)
+    is_active = models.BooleanField(default=True)
+
