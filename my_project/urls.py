4a425e9ed75ed7b6f281246be488bc925f92cad4
flexirexi <felix_lehmann@ymail.com>
2025-07-16 01:12:37 +0200

    feat: add profile page

--- my_project/urls.py
+++ my_project/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('user/', include('user.urls')),
 ]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
--- user/views.py
+++ user/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+from .forms import UserProfileForm
+from checkout.models import OrderItem
+
+
 # Create your views here.
+@login_required
+def profile(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    # Persönliche Daten bearbeiten
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserProfileForm(instance=profile)
+
+    # Alle OrderItems des Users
+    order_items = (
+        OrderItem.objects.filter(order__user=request.user)
+        .select_related('order', 'variant__product', 'variant__size', 'variant__product__occasion', 'variant__color', 'variant__fit')
+        .order_by('-order__created_at')
+    )
+
+    context = {
+        'form': form,
+        'order_items': order_items,
+    }
+    return render(request, 'user/profile.html', context)
